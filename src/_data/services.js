bedf25a1251f661a22d296b5c2df0064a1dbb66b
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2021-03-30 22:55:58 +0100

    service section

--- src/_data/services.js
+++ src/_data/services.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
-    title: 'Costumer Development',
+    title: 'Customer Development',
     details:
-      "Helping you understand customers and their needs that you may be able to satisfy. You cannot validate your assumtions on your own, I'll take you through a challenging process where we talk with customers to discover if your assumptions are true.",
+      'Let me help you understand prospective customers and their needs that you may be able to satisfy. You cannot validate your assumptions on your own, I’ll take you through a challenging process where we talk with customers to discover if your assumptions are true.',
     icon: 'bx-user-voice.svg',
   },
   {
     title: 'Hyper-convenient Design Thinking',
     details:
-      "I like to remove things that create friction and dristraction and this typically means a shorter route to a solution. Less is often  more. Whilst. I'm not a graphic designer I do think deeply about the lack of attention span people have when they're confronted  with new products or steps.",
+      'I like to remove things that create friction and distraction and this typically means a shorter route to a solution. Less is often more. Whilst I’m not a graphic designer I do think deeply about the lack of attention span people have when they’re confronted with new products or steps within those products.',
     icon: 'bx-brain.svg',
   },
   {
@@ -16,5 +16,23 @@ module.exports = [
     details:
       'Rarely is a marketplace as simple as supply and demand. And whilst the internet empowers us to remove middlemen there are always other stakeholders to consider. I can help you figure out the wider ecosystem that your product or marketplace operates in, and if, and how you could fuel adoption.',
     icon: 'bx-line-chart.svg',
+  },
+  {
+    title: 'Product Architecture',
+    details:
+      'The requirements are clear but there are many ways for the features and components of your product to fit into the overall system and business model. I take a user-centred approach to map the functions and features you require, to the form of your product.',
+    icon: 'bxl-codepen.svg',
+  },
+  {
+    title: 'Crowdfunding',
+    details:
+      'There’s nowhere to hide if you launch a crowdfunding campaign unprepared. The eager and eagled-eyed investors will pick apart every word, picture and number in your materials. I’ve had previous success with equity crowdfunding and I can help you get your ducks in a row.',
+    icon: 'bx-coin.svg',
+  },
+  {
+    title: 'Pitch Deck Development',
+    details:
+      'You have a blank canvas to communicate your ideas but you’re struggling to connect the creative side of your brain with the logical side. I can distill your vision and convert your messaging & content into a compelling and flowing narrative.',
+    icon: 'bx-slideshow.svg',
   },
 ];
